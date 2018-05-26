3b12dba2e6d31524349cd33979f5f3fe0a554752
PMASTUDIO <32707772+PMASTUDIO@users.noreply.github.com>
2018-05-26 18:37:30 -0300

    Sistema De Movimento
    Sendo Implementado

---
+++ Your Status (Alpha)/Assets/Sistemas/MovimentoPersonagemPrincipal/Andar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Andar : MonoBehaviour {
+	public Rigidbody2D self;
+	public float velocidade;
+	// Use this for initialization
+	void Start () {
+		self = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Forward ();
+	}
+
+	void Forward(){
+		if(Input.GetAxis("Vertical") > 0){
+			self.transform.position += new Vector3 (0, velocidade / 100, 0) * Time.deltaTime;
+		}
+		else if(Input.GetAxis("Vertical") < 0){
+			self.transform.position -= new Vector3 (0, velocidade / 100, 0) * Time.deltaTime;
+		}
+	}
+}
