3552103887abd775ab4044872b1334367266e9ee
PMASTUDIO <32707772+PMASTUDIO@users.noreply.github.com>
2018-07-06 12:05:50 -0300

    Stawker Finished

--- Your Status (Alpha)/Assets/Sistemas/Stawker/MoveEntity.cs
+++ Your Status (Alpha)/Assets/Sistemas/Stawker/MoveEntity.cs
@@ -2,28 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveEntity : MonoBehaviour {
-    public float speed;
-    private int screenWidth;
-    private int screenHeight;
-    private Vector3 dir;
-    // Use this for initialization
-    void Start () {
-        screenWidth = Screen.width;
-        screenHeight = Screen.height;        
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        if (transform.position.x >= 8)
+public class MoveEntity : MonoBehaviour
+    {
+        public float speed;
+        private Vector3 dir;
+        public bool stop; 
+        // Use this for initialization
+        void Start()
         {
-            dir = new Vector3(-speed, 0, 0);
-        }
-        else if (this.transform.position.x <= -8)
-        {
-            dir = new Vector3(speed, 0, 0);
         }
 
-        transform.position += dir;
-    }
+        // Update is called once per frame
+        void Update()
+        {
+            if(stop == true)
+            {
+                speed = 0;
+                dir = new Vector3(speed, 0, 0);
+            }
+            else
+            {
+                if (transform.position.x >= 8)
+                {
+                    dir = new Vector3(-speed, 0, 0);
+                }
+                else if (this.transform.position.x <= -8)
+                {
+                    dir = new Vector3(speed, 0, 0);
+                }
+            }
+
+            transform.position += dir;
+        }
+
 }
