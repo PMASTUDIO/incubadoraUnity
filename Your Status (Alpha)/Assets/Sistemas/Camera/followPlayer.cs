28242973951dfef0d3e72a8d6f146e495ef57a5b
PMASTUDIO <32707772+PMASTUDIO@users.noreply.github.com>
2018-07-10 11:44:49 -0300

    DayCounterAdded (Not Tested)

---
+++ Your Status (Alpha)/Assets/Sistemas/Camera/followPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followPlayer : MonoBehaviour {
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Set the camera position to be equals to the player position;
+        this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, this.transform.position.z);
+	}
+}
