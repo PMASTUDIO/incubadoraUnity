445391888cb22aefd911a4836dd1539d1a089535
PMASTUDIO <32707772+PMASTUDIO@users.noreply.github.com>
2018-07-17 11:23:38 -0300

    DayEndedScene

---
+++ Your Status (Alpha)/Assets/Sistemas/Progress/DayEnd.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayEnd : MonoBehaviour {
+
+    public GameObject missionGuy;
+    private float XpositionToWin;
+    private float YpositionToWin;
+    private float ZobligatedPosition;
+    public DayCounter dayCounterScriptObject;
+
+	// Use this for initialization
+	void Start () {
+        XpositionToWin = missionGuy.transform.position.x;
+        YpositionToWin = missionGuy.transform.position.y;
+        ZobligatedPosition = missionGuy.transform.position.z;
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, ZobligatedPosition);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if((this.transform.position.x >= (XpositionToWin - 2) && this.transform.position.x <= (XpositionToWin + 2)) && (this.transform.position.y >= (XpositionToWin - 2) && this.transform.position.y <= (XpositionToWin + 2)))
+        {
+            Debug.Log("ShowPanel");
+            dayCounterScriptObject.showPanel = true;
+        }
+	}
+
+}
