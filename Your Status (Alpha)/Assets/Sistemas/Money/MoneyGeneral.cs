0ecc0e3f2c2a05812d57c1c0c058ba8ef32cc1ec
PMASTUDIO <32707772+PMASTUDIO@users.noreply.github.com>
2018-07-18 12:10:16 -0300

    Money and Family

---
+++ Your Status (Alpha)/Assets/Sistemas/Money/MoneyGeneral.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyGeneral : MonoBehaviour {
+
+    public int Money;
+    private int ComputerMoney;
+    public Text lblMoney;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        try
+        {
+            ComputerMoney = PlayerPrefs.GetInt("money");
+            lblMoney.text = ComputerMoney.ToString();
+            Money = ComputerMoney + Money;
+        }
+        catch (System.Exception e)
+        {
+            PlayerPrefs.SetInt("money", Money);
+            lblMoney.text = ComputerMoney.ToString();
+            Money = ComputerMoney + Money;
+        }
+    }
+}
